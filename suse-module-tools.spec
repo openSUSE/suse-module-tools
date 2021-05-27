openSUSE/suse-module-tools

commit 4aab6a822b71bcb3d5a2367ffde6cf1d167a70da

Unify ppc64 and ppc64le

suse-module-tools.spec
@@ -128,7 +128,7 @@ install -pm 644 50-kernel-uname_r.conf "%{buildroot}%{_unitdir}/systemd-sysctl.s
 # Not needed in SLE11, where sg is loaded via udev rule.
 install -d -m 755 "%{buildroot}%{modules_load_dir}"
 install -pm 644 sg.conf "%{buildroot}%{modules_load_dir}"
-%ifarch ppc64le
+%ifarch ppc64 ppc64le
 install -d -m 755 %{buildroot}/usr/lib/systemd/system-generators
 install -m 755 udev-trigger-generator %{buildroot}/usr/lib/systemd/system-generators
 %endif

modprobe.conf/modprobe.conf.ppc64
@@ -1,0 +1,6 @@
+# begin of ppc64 part for modprobe.conf
+
+# catas recovery conflicts with eeh (bsc#456389)
+options ib_mthca catas_reset_disable=1
+
+# end of ppc64 part for modprobe.conf

modprobe.conf/modprobe.conf.ppc64le
@@ -1,6 +1,1 @@
-# begin of ppc64le part for modprobe.conf
-
-# catas recovery conflicts with eeh (bsc#456389)
-options ib_mthca catas_reset_disable=1
-
-# end of ppc64le part for modprobe.conf
+modprobe.conf.ppc64

udev-trigger-generator
@@ -13,7 +13,7 @@ export PATH=/usr/bin
 export TMPDIR=/dev/shm
 trap 'echo $0: error in "$BASH_COMMAND" >&2' ERR
 
-[[ $(uname -m) = ppc64le ]] || exit 0
+[[ $(uname -m) = ppc64le ]] || [[ $(uname -m) = ppc64 ]] || exit 0
 case $(systemd-detect-virt) in
     none|powervm) ;;
     *) exit 0;;

commit fe88f10478db030521d88036464ed750974933cf

spec file: add system-generators to %files for ppc64
Fixes: 4aab6a8 ("Unify ppc64 and ppc64le")

suse-module-tools.spec
@@ -259,7 +259,7 @@ done
 %{_unitdir}/systemd-sysctl.service.d
 %dir %{modules_load_dir}
 %{modules_load_dir}/sg.conf
-%ifarch ppc64le
+%ifarch ppc64 ppc64le
 /usr/lib/systemd/system-generators
 %endif
 